788738267318417cf575135baf7c058e5f5e4696
KubaPawlo <kuba.pawlo@wp.pl>
2025-04-03 21:27:11 +0200

    Initial commit

---
+++ src/main/java/lab/servletsjspjstl/ColorBean.java
@@ -0,0 +1,30 @@
+package lab.servletsjspjstl;
+
+public class ColorBean {
+    private String foregroundColor;
+    private String backgroundColor;
+    private boolean border;
+
+    public String getForegroundColor() {
+        return foregroundColor;
+    }
+
+    public void setForegroundColor(String foregroundColor) {
+        this.foregroundColor = foregroundColor;
+    }
+
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+    public boolean isBorder() {
+        return border;
+    }
+
+    public void setBorder(boolean border) {
+        this.border = border;
+    }
+}
